21b444052d29333f992110ddbace40a386906848
FlyDogDaDa <tp6ru4m03@gmail.com>
2022-07-06 16:02:35 +0800

    書開GUI

--- src/main/java/net/mcreator/magicbookzero/item/MagicbookItem.java
+++ src/main/java/net/mcreator/magicbookzero/item/MagicbookItem.java
@@ -1,6 +1,11 @@
 
 package net.mcreator.magicbookzero.item;
 
+import net.minecraftforge.network.NetworkHooks;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.TooltipFlag;
@@ -8,10 +13,26 @@ import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.nbt.CompoundTag;
+
+import net.mcreator.magicbookzero.world.inventory.MagicbookGUIMenu;
+import net.mcreator.magicbookzero.item.inventory.MagicbookInventoryCapability;
+
+import javax.annotation.Nullable;
 
 import java.util.List;
+
+import io.netty.buffer.Unpooled;
 
 public class MagicbookItem extends Item {
 	public MagicbookItem() {
@@ -32,5 +53,56 @@ public class MagicbookItem extends Item {
 	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, world, list, flag);
 		list.add(new TextComponent("Necessary medium used to activate magic"));
+	}
+
+	@Override
+	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
+		ItemStack itemstack = ar.getObject();
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		if (entity instanceof ServerPlayer serverPlayer) {
+			NetworkHooks.openGui(serverPlayer, new MenuProvider() {
+				@Override
+				public Component getDisplayName() {
+					return new TextComponent("Magicbook");
+				}
+
+				@Override
+				public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+					FriendlyByteBuf packetBuffer = new FriendlyByteBuf(Unpooled.buffer());
+					packetBuffer.writeBlockPos(entity.blockPosition());
+					packetBuffer.writeByte(hand == InteractionHand.MAIN_HAND ? 0 : 1);
+					return new MagicbookGUIMenu(id, inventory, packetBuffer);
+				}
+			}, buf -> {
+				buf.writeBlockPos(entity.blockPosition());
+				buf.writeByte(hand == InteractionHand.MAIN_HAND ? 0 : 1);
+			});
+		}
+		return ar;
+	}
+
+	@Override
+	public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable CompoundTag compound) {
+		return new MagicbookInventoryCapability();
+	}
+
+	@Override
+	public CompoundTag getShareTag(ItemStack stack) {
+		CompoundTag nbt = super.getShareTag(stack);
+		if (nbt != null)
+			stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)
+					.ifPresent(capability -> nbt.put("Inventory", ((ItemStackHandler) capability).serializeNBT()));
+		return nbt;
+	}
+
+	@Override
+	public void readShareTag(ItemStack stack, @Nullable CompoundTag nbt) {
+		super.readShareTag(stack, nbt);
+		if (nbt != null)
+			stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null)
+					.ifPresent(capability -> ((ItemStackHandler) capability).deserializeNBT((CompoundTag) nbt.get("Inventory")));
 	}
 }
